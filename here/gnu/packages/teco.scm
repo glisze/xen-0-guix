ff0f0c01c3a4c5b2c0b707af52510ff6489ad611
Gunter Liszewski (S8/linux-libre) <gunter.liszewski@gmail.com>
2020-01-02 00:24:01 +0000

    guix lint: teco is now in package teco
    Signed-off-by: Gunter Liszewski (S8/linux-libre) <gunter.liszewski@gmail.com>

--- here/gnu/packages/teco.scm
+++ here/gnu/packages/teco.scm
@@ -14,59 +14,63 @@
   #:use-module (guix packages))
 
 (define-public teco
-  (package 
-   (name "teco")
-   (version "0.0.72-b0a1")
-   (source (origin
-	    (method git-fetch)
-	    (uri (git-reference
-		  (url "https://github.com/glisze/teco.git")
-		  (commit "deploy/v0.0.72-b0a1")))
-	    (sha256
-	     (base32
-	    "1kmnyy687dzyk1zbilgfwnanhjfgnzdpcrzh6ycxc4j6ndvsmsan"))))
-   (build-system gnu-build-system)
-   (arguments
-    '(#:phases
-      (modify-phases %standard-phases
-		     (replace 'bootstrap
-			      (lambda _
-				(for-each (lambda (file)
-					    (patch-shebang file))
-					  (find-files "./scripts/"))
-				(invoke "sh" "-c" "echo 0.0.72-b0a1 > ./VERSION")
-				(invoke "sh" "-c" "touch ./ChangeLog")
-				(invoke "sh" "-c" "sed NEWS.in -e 1s,@VERSION[@],$( cat ./VERSION ), > NEWS")		
-				(invoke "sh" "-c" "autoreconf -vif"))))))
-   (propagated-inputs
-    `(("ncurses" ,ncurses)))
-   (native-inputs
-    `(("autoconf" ,autoconf)
-      ("automake" ,automake)
-      ("perl" ,perl)
-      ("git" ,git)
-      ("texinfo" ,texinfo)
-      ("pkg-config" ,pkg-config)))
-   (synopsis "Text Editor and COrrector program")
-   (description
-    "From the ibiblio archive of the @code{TECOC-146} program.
+  (package
+    (name "teco")
+    (version "0.0.72-b0a1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/glisze/teco.git")
+                    (commit "deploy/v0.0.72-b0a1")))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1kmnyy687dzyk1zbilgfwnanhjfgnzdpcrzh6ycxc4j6ndvsmsan"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (replace 'bootstrap
+           (lambda _
+             (for-each (lambda (file)
+                         (patch-shebang file))
+                       (find-files "./scripts/"))
+             (invoke "sh" "-c" "echo 0.0.72-b0a1 > ./VERSION")
+             (invoke "sh" "-c" "touch ./ChangeLog")
+             (invoke
+              "sh" "-c"
+              "sed NEWS.in -e 1s,@VERSION[@],$( cat ./VERSION ), > NEWS")
+             (invoke "sh" "-c" "autoreconf -vif"))))))
+    (propagated-inputs
+     `(("ncurses" ,ncurses)))
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("perl" ,perl)
+       ("git" ,git)
+       ("texinfo" ,texinfo)
+       ("pkg-config" ,pkg-config)))
+    (synopsis "Text Editor and COrrector program")
+    (description
+     "From the ibiblio archive of the @code{TECOC-146} program.
 
-This @code{teco} program has only minimal changes to get it built.
-Use with care!")
-   (home-page "https://glisze.github.com/teco")
-   (license gpl3+)))
+This @code{teco} program has only the minimal changes for it to built.
+Please, use it with due care!")
+    (home-page "https://glisze.github.com/teco")
+    (license gpl3+)))
 
 (define-public teco-local
-  (package (inherit teco)
-   (name "teco-local")
-   (version "0.0.72-b0a1")
-   (source (origin
-	    (method git-fetch)
-	    (uri (git-reference
-		  (url "file://home/gunter/source-5/guix/a/teco.git")
-		  (commit "deploy/v0.0.72-b0a1")))
-	    (sha256
-	     (base32
-	    "1kmnyy687dzyk1zbilgfwnanhjfgnzdpcrzh6ycxc4j6ndvsmsan"))))))
+  (package
+    (inherit teco)
+    (name "teco-local")
+    (version "0.0.72-b0a1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "file://home/gunter/source-5/guix/a/teco.git")
+                    (commit "deploy/v0.0.72-b0a1")))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1kmnyy687dzyk1zbilgfwnanhjfgnzdpcrzh6ycxc4j6ndvsmsan"))))))
 
-<
