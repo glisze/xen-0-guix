b2163e4ce8bf4a7313e95c1bcc1bf1429456768f
Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>
2021-11-19 03:35:28 +0000

    channel-0-here: introduction: preparing
    Signed-off-by: Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>

---
+++ etc/news.scm
@@ -0,0 +1,12 @@
+;; 20211119 (c) Gunter Liszewski -*- mode: scheme; -*-
+;; News: channel-0-here
+
+(channel-news
+ (version 0)
+
+ (entry (commit "a2afa28f2310ca18f449f98b7cb0b22543bfad76")
+        (title
+         (en "Introducing a new @command{channel}: @command{channel-0-here}"))
+        (body
+         (en "A separate channel with the packages from
+@command{xen-0-guix}."))))
