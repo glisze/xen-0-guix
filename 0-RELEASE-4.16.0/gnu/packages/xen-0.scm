3d637e5d772a658a8441d7780935a04013e44ecb
Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>
2022-02-16 02:36:26 +0000

    xen: 4.16.0: xen-0-boot: another half sideways
    Signed-off-by: Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>

--- 0-RELEASE-4.16.0/gnu/packages/xen-0.scm
+++ 0-RELEASE-4.16.0/gnu/packages/xen-0.scm
@@ -57,7 +57,8 @@
     (source (origin
               (method git-fetch)
               (uri (git-reference
-		    (url "/home/source-b/xen.org/xen")
+		    (url "git://xenbits.xenproject.org/xen.git")
+		    ;; (url "/home/source-b/xen.org/xen")
 		    (commit xen-commit)))
                     ;; (url "git://xenbits.xenproject.org/xen.git")
                     ;;(commit (string-append "RELEASE-" version))))
