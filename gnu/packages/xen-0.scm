5ce9d29030cb48e19de6d6f3d9cea69402451956
Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>
2022-03-03 01:18:19 +0000

    qemu-xen: 4.16.0: t97: configure-for-install fix
    Signed-off-by: Gunter Liszewski (@S10) <gunter.liszewski@gmail.com>

--- gnu/packages/xen-0.scm
+++ gnu/packages/xen-0.scm
@@ -589,8 +589,14 @@ override CC = " (assoc-ref inputs "cross-gcc") "/bin/i686-linux-gnu-gcc"))
            (lambda* (#:key inputs outputs (configure-flags '()) #:allow-other-keys)
              (let ((out (assoc-ref outputs "out")))
                (setenv "SHELL" (which "bash"))
-               (substitute* "tests/libqtest.c"
-                 (("/bin/sh") (which "sh")))
+               (substitute* '("block/cloop.c" "migration/exec.c"
+                              "net/tap.c" "tests/qtest/libqtest.c"
+                              "tests/qtest/vhost-user-blk-test.c")
+			    (("/bin/sh") (search-input-file inputs "/bin/sh")))
+	       (substitute* "tests/qemu-iotests/testenv.py"
+			    (("#!/usr/bin/env python3")
+			     (string-append "#!" (search-input-file (or native-inputs inputs)
+                                                       "/bin/python3"))))
                ;; (setenv "LDFLAGS" "-lrt") ;; XXX: see above
                (apply invoke
                       `("./configure"
